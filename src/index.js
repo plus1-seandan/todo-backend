287954d0bba587a49e93006d301add21fd41a216
Sean Dan <seandan@Seans-MBP.attlocal.net>
2022-07-15 22:11:14 -0500

    connect to database

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const { Pool, Client } = require("pg");
+
+const credentials = {
+  user: "postgres",
+  host: "localhost",
+  database: "todo",
+  password: "postgres",
+  port: 5432,
+};
 
 const main = async () => {
     const app = express();
     //add middleware
 
     app.use(cors());
-  
+    const pool = new Pool(credentials);
+
     app.listen(5000, () => {
       console.log(`🚀 Server ready at http://localhost:5000/`);
     });
 
-    app.get('/tasks', (req, res) => {
-        res.send('these are tasks')
+    app.get('/tasks', async (req, res) => {
+        const allTasks = await pool.query('SELECT * FROM tasks');
+        const resultData = allTasks.rows
+        res.status(200).send(resultData);
     });
   };
   
