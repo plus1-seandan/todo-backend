4c0ae615666a5e8ca1e902ea2d097656f1de010f
Sean Dan <seandan@Seans-MBP.attlocal.net>
2022-07-15 23:18:53 -0500

    added post route to add task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { Pool, Client } = require("pg");
+const bodyParser = require("body-parser");
 
 const credentials = {
   user: "postgres",
@@ -15,6 +16,9 @@ const main = async () => {
     //add middleware
 
     app.use(cors());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
     const pool = new Pool(credentials);
 
     app.listen(5000, () => {
@@ -25,6 +29,14 @@ const main = async () => {
         const allTasks = await pool.query('SELECT * FROM tasks');
         const resultData = allTasks.rows
         res.status(200).send(resultData);
+    });
+
+    app.post('/tasks', async (req, res) => {
+        const id = req.body.id; 
+        const description = req.body.description; 
+        await pool.query(`INSERT INTO tasks (id, description) VALUES (${id}, '${description}')`);
+
+        res.status(200).send(`Successfully saved task: ${description}`);
     });
   };
   
